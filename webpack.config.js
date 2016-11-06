cb823bbebe927d10bc2d1a7a395d44d4fe7e1b07
Pascal Pencil <pascal@superpencil.com>
2016-11-06 21:36:41 +0900

    SASS

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,20 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  devtool: 'source-map',
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }, {
+        test: /\.sass$/,
+        loaders: ['style', 'css', 'sass']
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
