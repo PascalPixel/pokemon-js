2d1276f9f40f9ff9651e64232d1b9fe52450bdb2
Pascal Pencil <pascal@superpencil.com>
2016-11-07 03:09:12 +0900

    TextArea

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import TextArea             from './text-area'
 
 require('../../style/main.sass')
 
@@ -111,17 +112,7 @@ export default class App extends Component {
             </div>
           </div>
         </div>
-        <div className="text-area">
-          <p>
-            Pokémon Mini
-            <br/>
-            by <a href="http://www.superpencil.com">Superpencil</a>
-          </p>
-          <p>Pokémon and Pikachu are trademarks of The Pokémon Company International</p>
-          <p>If you use this code please mention the original repository.</p>
-          <p>If you make improvements, please issue a pull request, we're very happy to have your involvement.</p>
-          <small>Author - Pascal Pixel</small>
-        </div>
+        <TextArea />
       </div>
     )
   }
