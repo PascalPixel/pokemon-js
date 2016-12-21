9e8758ebf7dfcec9bdd55e6a5ce92c2cf6eed1a5
Pascal Pencil <pascal@superpencil.com>
2016-12-21 15:51:16 +0900

    Avoid spaces in image 'format'

--- src/components/Sprite.js
+++ src/components/Sprite.js
@@ -1,46 +1,40 @@
 import React from 'react'
 import images from '../data/images'
 
-const colors = [
-  [' ', '#242803'],
-  ['░', '#4D5607'],
-  ['▒', '#7A8718'],
-  ['▓', null]
-]
-
 let Sprite = (props) => {
   let image = images[props.name][props.currentTrainer]
-  let length = image[0].length
+  let width = image[0].length
+  let height = image.length
 
   let pixels = []
-  for (let l = 0; l < length; l++) {
-    for (let p = 0; p < length; p++) {
+  for (let l = 0; l < height; l++) {
+    for (let p = 0; p < width; p++) {
       let color
       switch (image[l][p]) {
-        case colors[0][0]:
-          color = colors[0][1]
+        case '█':
+          color = '#242803'
           break
-        case colors[1][0]:
-          color = colors[1][1]
+        case '▓':
+          color = '#4D5607'
           break
-        case colors[2][0]:
-          color = colors[2][1]
+        case '▒':
+          color = '#7A8718'
           break
-        case colors[3][0]:
-          color = colors[3][1]
+        case '░':
+          color = null
           break
         default:
           color = '#FF0000'
       }
-      color ? pixels.push(<rect key={p + ' ' + l} x={p} y={l} width={1} height={1} fill={color} />) : null
+      color ? pixels.push(<rect key={l + '-' + p} x={p} y={l} width={1} height={1} fill={color} />) : null
     }
   }
 
   return (
     <svg
-      viewBox={`0 0 ${length} ${length}`}
-      xmlns='http://www.w3.org/2000/svg'>
-      <title>{props.name}_{props.currentTrainer}</title>
+      viewBox={`0 0 ${height} ${width}`}
+      xmlns='http://www.w3.org/2000/svg'
+      class={props.name + '_' + props.currentTrainer}>
       {pixels}
     </svg>
   )
