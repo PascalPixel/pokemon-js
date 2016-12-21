b62c97c6396d73273ae516aae435c4436b63dc4a
Pascal Pencil <pascal@superpencil.com>
2016-12-21 14:26:28 +0900

    Don't print empty svg 'pixels' and lines

--- src/components/Sprite.js
+++ src/components/Sprite.js
@@ -5,16 +5,15 @@ const colors = [
   [' ', '#242803'],
   ['░', '#4D5607'],
   ['▒', '#7A8718'],
-  ['▓', 'transparent']
+  ['▓', null]
 ]
 
 let Sprite = (props) => {
   let image = images[props.name][props.currentTrainer]
   let length = image[0].length
 
-  let lines = []
+  let pixels = []
   for (let l = 0; l < length; l++) {
-    let pixels = []
     for (let p = 0; p < length; p++) {
       let color
       switch (image[l][p]) {
@@ -33,9 +32,8 @@ let Sprite = (props) => {
         default:
           color = '#FF0000'
       }
-      pixels.push(<rect key={p + l} x={p} y={l} width={1} height={1} fill={color} />)
+      color ? pixels.push(<rect key={p + ' ' + l} x={p} y={l} width={1} height={1} fill={color} />) : null
     }
-    lines.push(<g key={l}>{pixels}</g>)
   }
 
   return (
@@ -43,7 +41,7 @@ let Sprite = (props) => {
       viewBox={`0 0 ${length} ${length}`}
       xmlns='http://www.w3.org/2000/svg'>
       <title>{props.name}_{props.currentTrainer}</title>
-      {lines}
+      {pixels}
     </svg>
   )
 }
