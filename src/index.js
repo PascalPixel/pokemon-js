0c342f8cea54af00a6eba3849f41da781a10b93f
Pascal Pencil <pascal@superpencil.com>
2016-12-22 18:59:32 +0900

    Moved SpriteMaker to pixels2ascii
    https://github.com/Superpencil/pixels2ascii

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Game from './containers/Game'
-// import SpriteMaker from './components/SpriteMaker'
 
 ReactDOM.render(<Game />, document.getElementById('pokemon'))
