1a741dfc4122eb63ab14305c4c8e1f8fadee3543
Pascal Pencil <pascal@superpencil.com>
2016-12-26 14:11:15 +0900

    Smaller build

--- webpack.config.js
+++ webpack.config.js
@@ -45,12 +45,12 @@ const development = merge(common, {
 
 const production = merge(common, {
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true,
-        warnings: false
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
       }
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ]
 })
 
