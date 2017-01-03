bd02c4c8e3e0c40b960bb65fa03aaa957cffb361
Pascal Verstegen <pascal@superpencil.com>
2017-01-03 17:39:44 +0900

    See, I'm stuck, so I'm just adding comments.

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -9,11 +9,14 @@ export default class Game extends React.Component {
   constructor (props) {
     super(props)
 
+    // Load store of info only in main Component.
     this.state = STORE
   }
 
-  // Functions
+  // Function that loops over array of functions with timeouts.
   animateArray (array) {
+    console.log('Animation started.')
+
     let offset = 0
     array.map((step) => {
       step[1] ? offset += step[1] : null
@@ -22,37 +25,52 @@ export default class Game extends React.Component {
         trigger()
       }, offset)
     })
+
+    console.log('Animation finished.')
   }
 
-  // Actions
+  // Changes that Windows are visible.
   reframe (update) {
+    console.log('Change visible windows: ' + Object.keys(update))
+
     const frames = Object.assign({}, this.state.frames, update)
     this.setState({frames})
   }
+
+  // Perform a move.
   attack (move) {
-    // Hide the Menu and Fight Menu
-    this.reframe({
-      fight: false,
-      menu: false
-    })
+    console.log('Move clicked: ' + move.name)
 
-    // Make State items readable
-    const player = this.state.trainers[this.state.allot.left]
-    const foe = this.state.trainers[this.state.allot.right]
-    const playerPokemonLevel = player.pokemon[player.activePokemon].level
-    const foePokemonHpBase = foe.pokemon[foe.activePokemon].hpBase
-    const foePokemonHpCurrent = foe.pokemon[foe.activePokemon].hpCurrent
-
-    // Images and Health bars
+    // Get all elements for animation; images and HP bars.
     const imagePlayer = document.getElementById('player').getElementsByClassName('images')[0]
     const imageFoe = document.getElementById('foe').getElementsByClassName('images')[0]
     const hpBar = document.getElementById('foe').getElementsByClassName('hp-bar-active')[0]
 
-    // New HP
-    const foePokemonHpUpdate = foePokemonHpBase - (Math.floor(((2 * playerPokemonLevel / 5 + 2) * move.power) / 50) + 2)
+    // Get the players.
+    const player = this.state.trainers[this.state.allot.left]
+    const foe = this.state.trainers[this.state.allot.right]
 
-    // Array with all animation steps
-    const attackAnimation = [
+    // Get details about the current Pokemon on both sides.
+    const playerPokemonLevel = player.pokemon[player.activePokemon].level
+    const foePokemonHpBase = foe.pokemon[foe.activePokemon].hpBase
+    const foePokemonHpCurrent = foe.pokemon[foe.activePokemon].hpCurrent
+
+    // New HP
+    const foePokemonHpUpdate = foePokemonHpBase - Math.floor((2 * playerPokemonLevel / 5 + 2) * move.power / 50 + 2)
+    console.log('Move damage: ' + foePokemonHpUpdate)
+
+    // Before Attacking.
+    function startAttack () {
+      // Hide the Menu and Fight Menu.
+      this.reframe({
+        fight: false,
+        menu: false
+      })
+    }
+
+    // Array with all steps of the attack, including start and end functions, second argument is delay before running.
+    this.animateArray([
+      [() => { startAttack() }, 0],
       [() => { imagePlayer.style.left = '0em' }, 100],
       [() => { imagePlayer.style.left = '1.5em' }, 100],
       [() => { imagePlayer.style.left = '0.8em' }, 100],
@@ -62,45 +80,59 @@ export default class Game extends React.Component {
       [() => { imageFoe.style.opacity = 1 }, 100],
       [() => { imageFoe.style.opacity = 0 }, 100],
       [() => { imageFoe.style.opacity = 1 }, 100],
-      [() => { hpBar.style.width = `${foePokemonHpUpdate * 100 / foePokemonHpBase}%` }, 100]
-    ]
-    this.animateArray(attackAnimation)
+      [() => { hpBar.style.width = `${foePokemonHpUpdate * 100 / foePokemonHpBase}%` }, 100],
+      [() => { endAttack() }, 500]
+    ])
 
-    // Update the State with new HP
-    this.setState({
-      [foePokemonHpCurrent]: foePokemonHpUpdate
-    })
+    // After Attacking.
+    function endAttack () {
+      // Update the State with new HP.
+      this.setState({
+        [foePokemonHpCurrent]: foePokemonHpUpdate
+      })
 
-    // Show Menu
-    this.reframe({ menu: true })
+      // Show Menu.
+      this.reframe({
+        menu: true
+      })
+
+      console.log('Move ' + move.name + ' finished.')
+    }
   }
+
+  // Switch Pokemon
   change (pokemon) {
-    console.log('switch: ' + pokemon.name)
+    console.log('Switch to: ' + pokemon.name)
   }
+
+  // Use Item
   use (item) {
-    console.log('use: ' + item.name)
+    console.log('Item used: ' + item.name)
   }
+
+  // Run from encounter/battle.
   run () {
-    console.log('run')
+    console.log('Attempted to Run.')
   }
 
+  // Render component.
   render () {
-    const player = this.state.trainers[this.state.allot.left]
-
     return (
-      <div className='depth'>
+      <div>
         <Trainer
           trainer={this.state.trainers[this.state.allot.right]}
           currentTrainer={0} />
+
         <Trainer
-          trainer={player}
+          trainer={this.state.trainers[this.state.allot.left]}
           currentTrainer={1} />
+
         <Windows
-          trainer={player}
+          trainer={this.state.trainers[this.state.allot.left]}
           frames={this.state.frames}
           lines={this.state.lines}
 
-          // Actions
+          // Actions.
           reframe={this.reframe.bind(this)}
           attack={this.attack.bind(this)}
           change={this.change.bind(this)}
